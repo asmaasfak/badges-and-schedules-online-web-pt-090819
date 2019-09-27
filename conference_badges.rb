4797b48f79c759c0322ae3fead59c02bb28d433a
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-27 23:38:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def badge_maker(name)
+  p "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  array_badge =[]
+  array.each do |name|
+      array_badge << "Hello, my name is #{name}."
+  end
+    array_badge
+end
+
+def assign_rooms (speakers)
+  room_assigned = []
+  speakers.each_with_index do |speaker, index|
+     room_assigned << "Hello, #{speaker}! You'll be assigned to room #{index}!"
+  end
+  room_assigned
+end
