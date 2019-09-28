fae39a79938c7b951c413a03144866a1d8914338
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-28 01:50:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,10 +19,10 @@ def assign_rooms (speakers)
 end
 
 def printer (attendees)
-  batch_badge_creator (attendees).each do |person|
+  batch_badge_creator(attendees).each do |person|
     puts person
   end
-  assign_rooms (attendees).each do |person|
+  assign_rooms(attendees).each do |person|
     puts person
   end
 end
